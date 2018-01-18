550b1f62b00449ed8933a7ed0965d35c8b97547b
cmthakur <chandra.thakur@cloudfactory.com>
2018-01-18 15:03:18 +0545

    add methods for health_monitor

--- lib/health_monitor.rb
+++ lib/health_monitor.rb
@@ -1,5 +1,24 @@
-require "health_monitor/version"
+require 'health_monitor/version'
+require 'health_monitor/service_loader'
+require 'health_monitor/supervisor'
 
 module HealthMonitor
-  # Your code goes here...
+  extend self
+
+  def load!
+    services = ServiceLoader.new.services.keys
+    services.each do |service|
+      require "health_monitor/services/#{service}"
+    end
+  end
+
+  def version
+    VERSION
+  end
+
+  def check(services = nil)
+    Supervisor.new(services).check
+  end
 end
+
+HealthMonitor.load!
